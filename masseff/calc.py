3046df71cde4f73bbaa96b19366d105debab6a6c
Wei Chen <waynechen@gmail.com>
2016-12-17 22:51:35 +0100

    Spin-polarization added

--- masseff/calc.py
+++ masseff/calc.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pymatgen.io.vasp.outputs import Vasprun
-from pymatgen.io.vasp.inputs import Kpoints
+from pymatgen.io.vasp.inputs import Kpoints, Poscar
 from pymatgen.io.vasp.sets import MPNonSCFSet
 from pymatgen.electronic_structure.bandstructure import Spin
 from os.path import join
@@ -37,27 +37,39 @@ class Mass_eff():
 
         return self.h*stcl
 
-    def kpoints(self, recp_vec, k0_recp):
+    def kpoints(self, recp_vec, k0_recp, a, out='rec'):
         '''List of kpoints for the nscf calculation
         in reciprocal lattice vectors; 
+        a: scaling factor found in POSCAR
         ''' 
         recp_vec_inv = np.linalg.inv(recp_vec)
         k0_cart = np.dot(k0_recp, recp_vec)
         kl_cart = k0_cart + self.stencil()
-        kl_recp = np.dot(kl_cart, recp_vec_inv)
+        if out == 'rec':
+            kl_recp = np.dot(kl_cart, recp_vec_inv)
+            list_k = kl_recp
+        else:
+            list_k = kl_cart/(2*np.pi/a)
+        return list_k
 
-        return kl_recp
-
-    def write_vasp(self, k0_recp, scfdir, destdir):
+    def write_vasp(self, k0_recp, scfdir, destdir, out='rec'):
         '''Generate VASP nscf input files
         '''
         r = Vasprun(join(scfdir, 'vasprun.xml'))
         recp_vec = r.lattice_rec.matrix
-        kl_recp = self.kpoints(recp_vec, k0_recp)
-        nscf = MPNonSCFSet.from_prev_calc(scfdir)
+        if not out == 'rec':
+            p = Poscar.from_file(join(scfdir, 'POSCAR'))
+            a = float(p.get_string().split()[1])
+        else:
+            a = 1.0
+        list_k = self.kpoints(recp_vec, k0_recp, a, out)
+        nscf = MPNonSCFSet.from_prev_calc(scfdir, user_incar_settings={"NCORE": 4, "EDIFF":"1E-7"})
         nscf.write_input(destdir)
-        kpt = Kpoints(style=Kpoints.supported_modes.Reciprocal, kpts=kl_recp,
-              num_kpts=len(kl_recp), kpts_weights=[1.0]*len(kl_recp),
+        if out == 'rec':
+            mode = Kpoints.supported_modes.Reciprocal
+        else:
+            mode = Kpoints.supported_modes.Cartesian
+        kpt = Kpoints(style=mode, kpts=list_k, num_kpts=len(list_k), kpts_weights=[1.0]*len(list_k),
               comment="5-point stencil h:{}".format(self.h))
         kpt.write_file(join(destdir, 'KPOINTS'))
 
@@ -66,42 +78,55 @@ class Mass_eff():
         e is a length-61 array (in eV).
         '''
         h = self.h
-        e = self.read_eigenvalues(type, destdir)
-
+        eig = self.read_eigenvalues(type, destdir)
+        mass = {}
+        for spin in eig.keys():
+            mass[spin] = self.mass_tensor(eig[spin], self.h)
+        
+        return mass
+    
+    @staticmethod
+    def mass_tensor(e,h):
         m_t = np.zeros((3,3)) # 3x3 tensor
-        m_t[0,0] = (-(e[1]+e[4])  + 16.0*(e[2]+e[3])   - 30.0*e[0])/(12.0*h**2)
-        m_t[1,1] = (-(e[5]+e[8])  + 16.0*(e[6]+e[7])   - 30.0*e[0])/(12.0*h**2)
-        m_t[2,2] = (-(e[9]+e[12]) + 16.0*(e[10]+e[11]) - 30.0*e[0])/(12.0*h**2)
-        m_t[0,1] = (-63.0*(e[15]+e[20]+e[21]+e[26]) + 63.0*(e[14]+e[17]+e[27]+e[24]) + 
-                     44.0*(e[16]+e[25]-e[13]-e[28]) + 74.0*(e[18]+e[23]-e[19]-e[22]))/(600.0*h**2)
-        m_t[0,2] = (-63.0*(e[31]+e[36]+e[37]+e[42]) + 63.0*(e[30]+e[33]+e[43]+e[40]) +
-                     44.0*(e[32]+e[41]-e[29]-e[44]) + 74.0*(e[34]+e[39]-e[35]-e[38]))/(600.0*h**2)
-        m_t[1,2] = (-63.0*(e[47]+e[52]+e[53]+e[58]) + 63.0*(e[46]+e[49]+e[59]+e[56]) +
-                     44.0*(e[48]+e[57]-e[45]-e[60]) + 74.0*(e[50]+e[55]-e[51]-e[54]))/(600.0*h**2)
-
+        m_t[0,0] = (-(e[1]+e[4])  + 16*(e[2]+e[3])   - 30*e[0])/(12*h**2)
+        m_t[1,1] = (-(e[5]+e[8])  + 16*(e[6]+e[7])   - 30*e[0])/(12*h**2)
+        m_t[2,2] = (-(e[9]+e[12]) + 16*(e[10]+e[11]) - 30*e[0])/(12*h**2)
+        m_t[0,1] = (-63*(e[15]+e[20]+e[21]+e[26]) + 63*(e[14]+e[17]+e[27]+e[24]) + 
+                     44*(e[16]+e[25]-e[13]-e[28]) + 74*(e[18]+e[23]-e[19]-e[22]))/(600*h**2)
+        m_t[0,2] = (-63*(e[31]+e[36]+e[37]+e[42]) + 63*(e[30]+e[33]+e[43]+e[40]) +
+                     44*(e[32]+e[41]-e[29]-e[44]) + 74*(e[34]+e[39]-e[35]-e[38]))/(600*h**2)
+        m_t[1,2] = (-63*(e[47]+e[52]+e[53]+e[58]) + 63*(e[46]+e[49]+e[59]+e[56]) +
+                     44*(e[48]+e[57]-e[45]-e[60]) + 74*(e[50]+e[55]-e[51]-e[54]))/(600*h**2)
         m_t[1,0] = m_t[0,1]
         m_t[2,0] = m_t[0,2]
         m_t[2,1] = m_t[1,2] 
 
         w, v = np.linalg.eig(m_t)
-        w /= 27.2116/1.8897**2 # -> atomic units
+        w /= 27.2114/1.8897**2 # -> atomic units
         m = 1.0/w
-        m_cond = 3.0/np.sum(w)
 
-        return m, m_cond
+        return m
 
-    @staticmethod
-    def read_eigenvalues(type, destdir):
+    def read_eigenvalues(self, type, destdir):
         r = Vasprun(join(destdir, 'vasprun.xml'))
-        bs = r.get_band_structure()
-        if type == 'hole':
-            ind = max(bs.get_vbm()['band_index'][Spin.up])
-            eig = r.eigenvalues[Spin.up][:,ind][:,0]
+        eig = {}
+        if r.is_spin:
+            spins = [Spin.up, Spin.down]
         else:
-            ind = min(bs.get_cbm()['band_index'][Spin.up]) 
-            eig = r.eigenvalues[Spin.up][:,ind][:,0]
+            spins = [Spin.up]
+
+        for spin in spins:
+            ind = self.extremum_band_index(r, spin)
+            eig[spin.name] = r.eigenvalues[spin][:,ind,0]
         
         return eig
+
+    @staticmethod
+    def extremum_band_index(r, spin):
+        bs = r.eigenvalues[spin]
+        ind = max(np.argwhere(bs[0][:,1] > 0))        
+
+        return ind
 
 if __name__ == '__main__':
     # example
@@ -114,5 +139,5 @@ if __name__ == '__main__':
     #
     # do a vasp nscf calculation
     #
-    m, m_cond = em.calc_mass('hole')  # determine the hole effective mass
-    print(h, m, m_cond)
+    m = em.calc_mass('hole')  # determine the hole effective mass
+    print(h, m)
